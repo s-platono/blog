dbe810767e3ca4295d5dd403fbb2c5b2cf5ba82a
Valentin <platonov@REKSOFT.RU>
2022-04-02 22:42:30 +0300

    clean up default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,11 @@
 <template>
   <div id="page" class="v-application v-application--is-ltr theme--light">
-    <!--    <Header/>
-        <main class="col-lg-12 mt-5 pt-5">
-          <Nuxt/>
-        </main>
-        <footer class="footer">
-          <div class="col-lg-8 offset-lg-2">
-            <p class="col-lg- offset-lg-10">
-              <a href="https://twitter.com/el_platono" class="font-bold hover:underline">
-                <font-awesome-icon :icon="['fa-brands', 'twitter']"/>
-              </a>
-              {{new Date().getFullYear()}}
-            </p>
-          </div>
-        </footer>-->
     <div class="v-application--wrap">
-      <core-app-bar/>
-      <core-drawer/>
-      <core-view/>
+      <core-app-bar />
+      <core-drawer />
+      <core-view />
       <social />
-      <core-footer/>
+      <core-footer />
     </div>
   </div>
 </template>
