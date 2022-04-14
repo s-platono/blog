6a200a9f8c24f4d9f6e6b17a0505596e89b91766
s-platono <s-platono@users.noreply.github.com>
2022-04-14 18:55:17 +0000

    deploy: 69161d87bfdb956e94d4dcce9a53cad22b4f47dc

---
+++ _nuxt/static/1649962509/article/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649962509",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Farticle",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c4ff037a"}}}}(null));
